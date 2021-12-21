3a6cedd2f53f6c3e53e83287ed3862d6adc45c63
Destro <boly.destroer@mail.ru>
2021-12-21 15:49:45 +0800

    Правка

--- Kyrsovai/Form1.cs
+++ Kyrsovai/Form1.cs
@@ -10,45 +10,76 @@ using System.Windows.Forms;
 
 namespace Kyrsovai
 {
+
     public partial class Form1 : Form
     {
-        List<Particle> particles = new List<Particle>();
+        List<Emitter> emitters = new List<Emitter>();
+        Emitter emitter; // добавим поле для эмиттера
+        
+        
         public Form1()
         {
             InitializeComponent();
-            // привязал изображение
             picDisplay.Image = new Bitmap(picDisplay.Width, picDisplay.Height);
-            // генерирую 500 частиц
-            for (var i = 0; i < 500; ++i)
+
+            this.emitter = new Emitter // создаю эмиттер и привязываю его к полю emitter
             {
-                var particle = new Particle();
-                // переношу частицы в центр изображения
-                particle.X = picDisplay.Image.Width / 2;
-                particle.Y = picDisplay.Image.Height / 2;
-                // добавляю список
-                particles.Add(particle);
-            }
+                Direction = 0,
+                Spreading = 10,
+                SpeedMin = 10,
+                SpeedMax = 10,
+                ColorFrom = Color.Gold,
+                ColorTo = Color.FromArgb(0, Color.Red),
+                ParticlesPerTick = 10,
+                X = picDisplay.Width / 2,
+                Y = picDisplay.Height / 2,
+            };
+
+            
+          emitters.Add(this.emitter); // все равно добавляю в список emitters, чтобы он рендерился и обновлялся
+
+            emitter.Krug.Add(new Point(
+            picDisplay.Width / 2, picDisplay.Height / 2
+                
+        )) ;
+
         }
-        int counter = 0;
+
+
+
+
+       
+       
+        
+        
         private void timer1_Tick(object sender, EventArgs e)
         {
-            counter++; // увеличиваю значение счетчика каждый вызов
+            emitter.UpdateState(); //обновляем эмиттер
+
             using (var g = Graphics.FromImage(picDisplay.Image))
             {
-                // рисую на изображении сколько насчитал
-                g.DrawString(
-                    counter.ToString(), // значения счетчика в виде строки
-                    new Font("Arial", 12), // шрифт
-                    new SolidBrush(Color.Black), // цвет
-                    new PointF
-                    { // по центру экрана
-                        X = picDisplay.Image.Width / 2,
-                        Y = picDisplay.Image.Height / 2
-                    }
-                );
+                g.Clear(Color.Black);
+                emitter.Render(g); // рендерим систему
+                
             }
+            
+
             picDisplay.Invalidate();
         }
-    }
+        List<Particle> particles = new List<Particle>();
 
+        private void trackBar2_Scroll(object sender, EventArgs e)
+        {
+            emitter.Direction = tbDirection.Value; // направлению эмиттера присваиваем значение ползунка 
+            lblDirection.Text = $"{tbDirection.Value}°"; // добавил вывод значения
+        }
+        // Размер окружности
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            emitter.Power = tbPower.Value;
+            lblPower.Text = $"{tbPower.Value}R";
+        }
+    }
+    
+   
 }
